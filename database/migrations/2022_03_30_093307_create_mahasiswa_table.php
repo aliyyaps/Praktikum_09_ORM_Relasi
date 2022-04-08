b7f5a847963aec869a2771aa6f972764f4e41446
aliyyaps <aliyyaputris23@gmail.com>
2022-04-08 11:04:39 +0700

    Source Code dari Praktikum 7

---
+++ database/migrations/2022_03_30_093307_create_mahasiswa_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMahasiswaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mahasiswa', function (Blueprint $table) {
+            $table->id();
+            $table->string('nim', 10)->index();
+            $table->string('nama', 25)->index();
+            $table->string('kelas', 5);
+            $table->string('jurusan', 35);
+
+            // Tugas Praktikum 7 No 1
+            $table->string('email', 100);
+            $table->string('alamat', 100);
+            $table->date('tanggal_lahir');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mahasiswa');
+    }
+}
