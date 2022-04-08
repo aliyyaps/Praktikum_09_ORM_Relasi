39158b21dfddd2ef9586dc174e98464333aff593
aliyyaps <aliyyaputris23@gmail.com>
2022-04-08 11:27:29 +0700

    Mengubah di dalam Fungsi Index menjadi With

--- app/Http/Controllers/MahasiswaController.php
+++ app/Http/Controllers/MahasiswaController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Mahasiswa;
 use Illuminate\Support\Facades\DB;
+use App\Models\Kelas;
 
 class MahasiswaController extends Controller
 {
