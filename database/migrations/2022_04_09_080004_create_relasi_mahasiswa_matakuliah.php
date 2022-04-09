5f0d89e76a8731c98920a5796c729f99253ccafe
aliyyaps <aliyyaputris23@gmail.com>
2022-04-09 15:15:39 +0700

    Membuat Relasi Mahasiswa dengan Matakuliah

---
+++ database/migrations/2022_04_09_080004_create_relasi_mahasiswa_matakuliah.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRelasiMahasiswaMatakuliah extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('relasi_mahasiswa_matakuliah', function (Blueprint $table) {
+            $table->id();
+            //foreign key dari tabel mahasiswa
+            $table->string('mahasiswa_id', 10);
+            $table->foreign('mahasiswa_id')->references('nim')->on('mahasiswa')->onDelete('cascade');
+
+            //foreign key dari tabel matakuliah
+            $table->unsignedBigInteger('matakuliah_id');
+            $table->foreign('matakuliah_id')->references('id')->on('matakuliah')->onDelete('cascade');
+            $table->string('nilai', 1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('relasi_mahasiswa_matakuliah');
+    }
+}
